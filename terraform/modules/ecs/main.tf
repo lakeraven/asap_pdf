fb68fad24973f5d8bd7848050e93ba27ee463aee
Michael Cowden <michael.cowden@gmail.com>
2025-03-19 13:49:24 -0400

    getting port mappings straight

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -53,6 +53,7 @@ resource "aws_ecs_task_definition" "app" {
       portMappings = [
         {
           containerPort = var.container_port
+          hostPort      = var.container_port
           protocol      = "tcp"
         }
       ]
