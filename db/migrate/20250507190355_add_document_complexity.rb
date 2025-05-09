12f8fea4329a8c3b9a7713aa4b560c4b5d4ae2cb
Leo Kacenjar <lkacenja@gmail.com>
2025-05-09 14:46:34 -0600

    Remove old migration. Run new rake commands when setting up a new dev db.

--- db/migrate/20250507190355_add_document_complexity.rb
+++
@@ -1,5 +0,0 @@
-class AddDocumentComplexity < ActiveRecord::Migration[8.0]
-  def change
-    add_column :documents, :complexity, :text
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@ require "rake"
 if Rails.env.development?
   Rake::Task["users:create_admin"].invoke("password")
   Rake::Task["documents:bootstrap"].invoke
+  Rake::Task["documents:update_department"].invoke("1")
+  Rake::Task["documents:add_pdf_complexity"].invoke
 end
