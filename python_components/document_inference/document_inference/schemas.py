6c9454abe554d55439de4754c05dc2c2a68c8e8b
Allison Morgan <allison.morgan@gmail.com>
2025-06-25 15:45:31 -0700

    Improve LLM prompting (#188)
    * Remove third-party exception check
    
    * Support Gemini 2.5 Flash as evaluation model
    
    * Tweak archival prompt to include created date and update UI language
    
    * Pass document created date to evaluation lambda
    
    * Add created date to event validation and update log to be more generic inference
    
    * Change payload to reference creation_date vs created_date
    
    * Add examples to pre-existing document exception prompt
    
    * Remove LLM confidences

--- python_components/document_inference/document_inference/schemas.py
+++ python_components/document_inference/document_inference/schemas.py
@@ -11,17 +11,11 @@ class DocumentSummarySchema(BaseModel):
 class DocumentRecommendation(BaseModel):
     is_individualized: SkipJsonSchema[bool] = None
     why_individualized: SkipJsonSchema[str] = None
-    is_individualized_confidence: SkipJsonSchema[float] = None
     is_archival: bool = Field(
         description="Whether the document meets exception 1: Archived Web Content Exception"
     )
     why_archival: str = Field(
         description="An explanation of why the document meets or does not meet exception 1: Archived Web Content Exception"
-    )
-    is_archival_confidence: float = Field(
-        description="Percentage representing how confident you are about whether the document meets exception 1: Archived Web Content Exception",
-        ge=0,
-        le=1,
     )
     is_application: bool = Field(
         description="Whether the document meets exception 2: Preexisting Conventional Electronic Documents Exception"
@@ -29,19 +23,5 @@ class DocumentRecommendation(BaseModel):
     why_application: str = Field(
         description="An explanation of why the document meets or does not meet exception 2: Preexisting Conventional Electronic Documents Exception"
     )
-    is_application_confidence: float = Field(
-        description="Percentage representing how confident you are about whether the document meets exception 2: Preexisting Conventional Electronic Documents Exception",
-        ge=0,
-        le=1,
-    )
-    is_third_party: bool = Field(
-        description="Whether the document meets exception 3: Content Posted by Third Parties Exception"
-    )
-    why_third_party: str = Field(
-        description="An explanation of why the document meets or does not meet exception 3: Content Posted by Third Parties Exception"
-    )
-    is_third_party_confidence: float = Field(
-        description="Percentage representing how confident you are about whether the document meets exception 3: Content Posted by Third Parties Exception",
-        ge=0,
-        le=1,
-    )
+    is_third_party: SkipJsonSchema[bool] = None
+    why_third_party: SkipJsonSchema[str] = None
